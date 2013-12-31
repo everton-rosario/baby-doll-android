ee0a9cc2bda96e3686c8fece3badecf94f3a778e
Everton Rosario <erosario@gmail.com>
2013-12-31 17:46:47 -0200

    splash screen with transition

--- BabyDoll/src/main/java/com/rosarioinc/babydoll/SplashScreenActivity.java
+++ BabyDoll/src/main/java/com/rosarioinc/babydoll/SplashScreenActivity.java
@@ -1,18 +1,19 @@
 package com.rosarioinc.babydoll;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
 import com.crashlytics.android.Crashlytics;
 
 public class SplashScreenActivity extends Activity {
+
+    private static final long SPLASH_DISPLAY_TIME = 2000l;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,28 +27,24 @@ public class SplashScreenActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+
+                Intent intent = new Intent();
+                intent.setClass(SplashScreenActivity.this, MainActivity.class);
+
+                SplashScreenActivity.this.startActivity(intent);
+                SplashScreenActivity.this.finish();
+
+                // transition from splash to main menu
+                overridePendingTransition(R.anim.activity_main_fadein,
+                        R.anim.activity_splash_fadeout);
+
+            }
+        }, SPLASH_DISPLAY_TIME);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     /**
      * A placeholder fragment containing a simple view.
